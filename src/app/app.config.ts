fbf3e27e672c927e33f4e511747238c9ddeae20a
Flavio Alibeaj <flavio@dev.al>
2025-08-25 08:40:13 +0200

    feat: update snackbar options and improve error messages in file upload component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +18,12 @@ export const appConfig: ApplicationConfig = {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
       useValue: {
         appearance: 'outline',
+      },
+    },
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useValue: {
+        duration: 5000,
       },
     },
   ],
